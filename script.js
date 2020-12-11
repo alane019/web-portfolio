1b6f6d239fe7e0ceaf28f6a5e84e8e0b936db940
Adam <alane019@gmail.com>
2020-12-10 21:24:20 -0500

    updated top nav elements

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-onload = function(){
+$(document).ready(function() {
 
 
-	window.alert("OK");
-}
+	console.log("script.js");
+
+
+	$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+	$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+	$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+
+
+	// listen for save button clicks
+	$(".nav-link-div-container").on("click", function() {
+	
+		if($(this).hasClass("currentTab")){ return false;  }
+		else if($(this).attr('id') == 'contact') {window.location.href = "contact.html"}
+		else if($(this).attr('id') == 'portfolio') {window.location.href = "portfolio.html"}
+		else if($(this).attr('id') == 'index') {window.location.href = "index.html"}
+		
+	});
+
+  
+  });
+  
